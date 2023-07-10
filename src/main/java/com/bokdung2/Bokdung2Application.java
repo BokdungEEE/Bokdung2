870b9bfeb7be3aeaf231e7195ceb6a4b031a4523
minjung <ypd06021@naver.com>
2023-07-10 18:01:21 +0900

    feat: 이니셜커밋& 샘플컨트롤러

---
+++ src/main/java/com/bokdung2/Bokdung2Application.java
@@ -0,0 +1,13 @@
+package com.bokdung2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bokdung2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bokdung2Application.class, args);
+	}
+
+}
